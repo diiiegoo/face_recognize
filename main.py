f00e66f345be2cf481f3bfcf28fb63baf234181a
diiiegoo <63569474+diiiegoo@users.noreply.github.com>
2021-10-27 13:37:47 +0900

    1stcommit

---
+++ main.py
@@ -0,0 +1,40 @@
+from typing import Tuple
+import streamlit as st
+import requests
+import io
+from PIL import Image
+from PIL import ImageDraw
+import json
+
+
+st.title('顔認証アプリ')
+
+subscription_key = '6e56b74341f34d4fae60e06b66b85061'
+assert subscription_key
+face_api_url = 'https://1026streamlit.cognitiveservices.azure.com/face/v1.0/detect'
+
+uploaded_file = st.file_uploader("choose an image...", type='jpg')
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    with io.BytesIO() as output:
+        img.save(output, format="JPEG")
+        binary_img = output.getvalue()
+    headers = {
+        'Content-Type':'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': subscription_key}
+
+    params = {
+        'returnFaceId': 'true',
+        'returnFaceAttributes':'accessories,age,blur,emotion,exposure,facialhair,gender,glasses,hair,headpose,makeup,noise,occlusion,smile'
+    
+    }
+
+    res = requests.post(face_api_url, params=params,
+                         headers=headers, data=binary_img)
+    results = res.json()
+    for result in results:
+        rect = result['faceRectangle']
+        draw = ImageDraw.Draw(img)
+        draw.rectangle([(rect['left'],rect['top']),(rect['left']+rect['width'],rect['top']+rect['height'])],fill =None,width = 5,outline ='green')
+    st.image(img, caption='Uploaded Image.',use_column_width=True)
